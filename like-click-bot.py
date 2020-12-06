706d07aae54a26d9a146bcd9cee55ac4a29a00e9
don <don@mac.local>
2020-12-06 21:42:31 +0900

    ADD feature - login & search keyworld

--- like-click-bot.py
+++ like-click-bot.py
@@ -1,7 +1,9 @@
 import yaml
+import time
+from selenium import webdriver
 
 # get account config
-with open(r'/Users/don/Dropbox/git/temp-click-bot/config.yml') as file:
+with open(r'./config.yml') as file:
     documents = yaml.full_load(file)
 
     for key, value in documents.items():
@@ -11,4 +13,30 @@ with open(r'/Users/don/Dropbox/git/temp-click-bot/config.yml') as file:
             pw = value
 
 
-        
+def login(id, pw, driver):
+    # goto login page
+    driver.get('https://nid.naver.com/nidlogin.login')
+    time.sleep(1)
+
+    # input id, password
+    driver.execute_script("document.getElementsByName('id')[0].value=\'" + id + "\'")
+    time.sleep(1)
+    driver.execute_script("document.getElementsByName('pw')[0].value=\'" + pw + "\'")
+    time.sleep(1)
+
+    # click login button
+    driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+    time.sleep(1)
+
+def find_latest_blog(keyword, driver):
+    search_keywords = 'https://search.naver.com/search.naver?where=blog&sm=tab_opt&query=' + keyword + '&dup_remove=1&post_blogurl=&post_blogurl_without=&nso=so%3Add%2Ca%3Aall%2Cp%3Aall'
+    driver.get(search_keywords)
+    time.sleep(1)
+
+
+driver = webdriver.Chrome('./chromedriver')
+login(id, pw, driver)
+find_latest_blog('키보드 리뷰', driver)
+
+
+
